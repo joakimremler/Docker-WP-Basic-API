9eaf1273789208711078de973b2db177dd2545f5
Joakim <joakim@idrottskoll.se>
2018-01-04 12:31:12 +0100

    edit bash

--- setup.sh
+++ setup.sh
@@ -14,15 +14,16 @@ sudo chown -R $(whoami) wp-content/themes/
 # # Clone repository
 cd wp-content/themes
 git clone git@github.com:joakimremler/WP_Theme-Basic-api.git
+cd ../..
 
 #remove plugin: 
 echo "Remove plugins"
-sudo rm /wp-content/plugins/hello.php
+sudo rm wp-content/plugins/hello.php
 
 #remove themes
 echo "Remove themes"
-sudo rm -rf /wp-content/themes/twentyfifteen/
-sudo rm -rf /wp-content/themes/twentysixteen/
-sudo rm -rf /wp-content/themes/twentyseventeen/
+sudo rm -rf wp-content/themes/twentyfifteen/
+sudo rm -rf wp-content/themes/twentysixteen/
+sudo rm -rf wp-content/themes/twentyseventeen/
 
 
--- stop.sh
+++ stop.sh
@@ -5,9 +5,3 @@ docker stop $(docker ps -a -q)
 
 #remove all dockers instants
 docker rm $(docker ps -a -q)
-
-#remove all dockers (Varning!)
-# docker rmi $(docker images -q)
-
-#delete volumes
-docker volume rm `docker volume ls -q -f dangling=true`
